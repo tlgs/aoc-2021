6c51e9d6478802e3582829fc500d46c5099fa3c5
Tiago Seabra <tlgs@users.noreply.github.com>
2021-12-29 15:30:27 +0000

    chore: add template

---
+++ template.py
@@ -0,0 +1,36 @@
+"""Day X: """
+import sys
+
+
+def parse_input(puzzle_input):
+    ...
+
+
+def part_one():
+    ...
+
+
+def part_two():
+    ...
+
+
+class Test:
+    example = """\
+"""
+
+    def test_one(self):
+        ...
+
+    def test_two(self):
+        ...
+
+
+def main():
+    puzzle = parse_input(sys.stdin.read())
+
+    print("part 1:", part_one(puzzle))
+    print("part 2:", part_two(puzzle))
+
+
+if __name__ == "__main__":
+    main()
